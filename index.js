e29e2e9ebb526d72087152dbac0bdcfb0ed4a27b
Lrd134 <larryc3200@gmail.com>
2021-09-25 14:08:09 -0400

    commit tree 7ce461dc3c7eba7b32474469462d6f78d7bc6766

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your Circle class here
+class Circle {
+  constructor (radius) {
+    this.radius = radius;
+  }
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+  set circumference(circum) {
+    this.radius = circum / (2 * Math.PI);
+    return this.radius;
+  }
+  set diameter(dia) {
+    this.radius = dia / 2;
+    return this.radius;
+  }
+  set area(area) {
+    this.radius = Math.sqrt(area / Math.PI);
+    return this.radius;
+  }
+}
